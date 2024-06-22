e448018ef0d683092bfd41370412fca70f91cc6a
axatbhardwaj <axatbhardwaj@outlook.com>
2024-06-22 21:19:33 +0530

    spicetify config

---
+++ helpers/spicetify.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#this file is for configuring spiceify
+
+#make sure spicetify-cli is installed
+if ! command -v spicetify &> /dev/null
+then
+    echo "Spicetify not found. Installing Spicetify..."
+    paru -S spicetify-cli --noconfirm
+fi
+
+#permission fix
+sudo chmod a+wr /opt/spotify
+sudo chmod a+wr /opt/spotify/Apps -R
+
+#pref path
+spicetify config pref_path $HOME/.config/spotify/prefs
+
+#backup
+spicetify backup
+#installing spicetify-themes
+
+echo "Installing Spicetify Themes..."
+
+git clone --depth=1 https://github.com/spicetify/spicetify-themes.git
+
+cd spicetify-themes
+cp -r * ~/.config/spicetify/Themes
+
+#apply theme
+spicetify backup apply
