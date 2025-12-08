e00192834e462a28f18c3236adf3a81a67f883fb
axatbhardwaj <axatbhardwaj@outlook.com>
2025-12-08 21:41:23 +0530

    feat: add automated Dashy installation and bump version to 2.1.1

--- haoshoku.js
+++ haoshoku.js
@@ -12,7 +12,7 @@ const program = new Command();
 program
   .name("haoshoku")
   .description("Haoshoku: Color of the Supreme King. Dominate your setup.")
-  .version("2.0.0");
+  .version("2.1.1");
 
 function detectOS() {
   try {
