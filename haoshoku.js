fb0965a26a67cb28577ec691d6f0996b3e0935ab
axatbhardwaj <axatbhardwaj@outlook.com>
2026-01-22 00:12:25 +0530

    chore: release v2.10.0

--- haoshoku.js
+++ haoshoku.js
@@ -14,7 +14,7 @@ const program = new Command();
 program
 	.name("haoshoku")
 	.description("Haoshoku: Color of the Supreme King. Dominate your setup.")
-	.version("2.8.1")
+	.version("2.10.0")
 	.addHelpText("before", getBanner());
 
 function detectOS() {
