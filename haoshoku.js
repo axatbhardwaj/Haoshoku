66e1dae067c04d22985ade5cebaf461166389e2e
Axat Bhardwaj <axatbhardwaj@outlook.com>
2026-08-05 02:22:30 +0530

    chore: release v7.2.0

--- haoshoku.js
+++ haoshoku.js
@@ -51,7 +51,7 @@ const program = new Command();
 program
 	.name("haoshoku")
 	.description("Haoshoku: portable setup for Arch / Omarchy and Debian Server.")
-	.version("7.1.0")
+	.version("7.2.0")
 	.addHelpText("before", getBanner());
 
 program
