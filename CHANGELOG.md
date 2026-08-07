5e7df5f5893e50079ffff69cb9c1e81e562da049
Axat Bhardwaj <axatbhardwaj@outlook.com>
2026-08-07 10:17:27 +0530

    chore: release v7.7.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 7.7.0 - 2026-08-07
 
 - Make `Super+A` open a local kitty terminal rooted at home instead of attaching
   to a managed Remote Control session. A configured, syntactically valid Claude
--- haoshoku.js
+++ haoshoku.js
@@ -57,7 +57,7 @@ const program = new Command();
 program
 	.name("haoshoku")
 	.description("Haoshoku: portable setup for Arch / Omarchy and Debian Server.")
-	.version("7.6.0")
+	.version("7.7.0")
 	.addHelpText("before", getBanner());
 
 program
