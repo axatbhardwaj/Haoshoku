4a4dd5215ba9396cbb497ecdd51e1ff79831ef4f
Axat Bhardwaj <axatbhardwaj@outlook.com>
2026-08-15 16:35:27 +0530

    chore: release v8.6.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,6 @@
 # Changelog
 
-## Unreleased
+## 8.6.0 - 2026-08-15
 
 - Add the fixed, bubblewrap-confined OpenCode GLM seat with receipt and
   workspace-attribution enforcement for wrapper-dispatched work.
--- haoshoku.js
+++ haoshoku.js
@@ -62,7 +62,7 @@ const program = new Command();
 program
 	.name("haoshoku")
 	.description("Haoshoku: portable setup for Arch / Omarchy and Debian Server.")
-	.version("8.5.4")
+	.version("8.6.0")
 	.addHelpText("before", getBanner());
 
 program
